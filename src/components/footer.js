29b5298ffcd6d92b77ed539a2da71e11d11ff827
Stefan Kostoski <72026527+stefankostoski@users.noreply.github.com>
2022-06-12 21:38:50 +0200

    changes to footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,30 +3,80 @@ const footerTemplate = document.createElement('template');
 footerTemplate.innerHTML = `
     <style>
         footer{
-            width:100%;
-            height:61px;
-            background-color:var(--secondary-color);
-            display: flex;
+            padding: 20px 0px;
+            background-color:${getComputedStyle(document.querySelector(':root')).getPropertyValue('--secondary-color')};
+            display:flex;
+            flex-direction:row;
             align-items: center;
-            justify-content: center;
+            justify-content: space-around;
         }
 
-        small{
-            font-size:12px;
+        .nav-list{
+            list-style:none;
         }
 
-        @media screen and (min-width:760px){
+        .nav-list li{
+            display: inline;
+            padding-right: 10px;
+        }
+
+        .nav-list li a{
+            text-decoration:none;
+            font-size: 14px;
+        }
+
+        .copyright p{
+            font-size:14px;
+        }
+        
+        @media screen and (max-width: 600px){
             footer{
-                height:88px;
+                padding: 10px 0px;
+                flex-direction: column;
             }
-            small{
-                font-size:15px;
+
+            .copyright{
+                order:2;
+            }
+
+            .copyright p{
+                font-size:12px;
+            }
+
+            .footer-nav-links{
+                order:1;
+            }
+
+            .nav-list{
+                list-style:none;
+            }
+
+            ul{
+                padding-inline-start: 0px;
+            }
+
+            .nav-list li{
+                display: inline;
+                padding-right: 10px;
+
+            }
+
+            .nav-list li a{
+                text-decoration:none;
+                font-size:12px;
             }
         }
     </style>
 
     <footer>
-       <small>&copy;2022 NEWSTOPIA. ALL RIGHTS RESERVED.</small>
+        <div class="copyright"><p>&copy;2022 NEWSTOPIA. All rights reserved.</p></div>
+        <div class="footer-nav-links">
+            <ul class="nav-list">
+                 <li class="list-item"><a href="#">Privacy Policy</a></li>
+                 <li class="list-item"><a href="#">Cookies</a></li>
+                 <li class="list-item"><a href="#">About</a></li>
+            </ul>
+        </div>
     </footer>
 `;
 
