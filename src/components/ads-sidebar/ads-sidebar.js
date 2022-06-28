8bba06f87dae7059d348a32b8f24d53b7f5026d9
David Pavlovski <david.pavlovski@hotmail.com>
2022-06-28 15:04:52 +0200

    changed child selector in ads-sidebar.js

--- src/components/ads-sidebar/ads-sidebar.js
+++ src/components/ads-sidebar/ads-sidebar.js
@@ -14,8 +14,8 @@ class AdsSidebar extends HTMLElement {
       [ ...adsLinks ].forEach((el) => {
          const ad = ads[Math.floor(Math.random() * ads.length)];
          el.setAttribute('href', ad.redirect_url);
-         el.children[0].setAttribute('src', ad.image_url);
-         el.children[0].setAttribute('alt', ad.alt_text);
+         el.firstElementChild.setAttribute('src', ad.image_url);
+         el.firstElementChild.setAttribute('alt', ad.alt_text);
       });
    }
 }
