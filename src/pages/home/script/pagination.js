50f70dce27990baee4cc1ba3476c3c47094ab6f0
David Pavlovski <david.pavlovski@hotmail.com>
2022-06-30 19:01:27 +0200

    removed target blank from article link

--- src/pages/home/script/pagination.js
+++ src/pages/home/script/pagination.js
@@ -19,12 +19,12 @@ function populateHomepage(data, page, pageSize, target){
                     <div class="content">
                         <h3 class="article-title">${results.title}</h3>
                         <p>${results.description}</p>
-                        <a href="../article/article.html?id=${results.id}/" class="btn-link" target="_blank">Read more</a>
+                        <a href="../article/article.html?id=${results.id}/" class="btn-link">Read more</a>
                     </div>
             </section>
         `;
-   };
-};
+   }
+}
 
 function getCurrentPage(){
    const hash = window.location.hash;
@@ -35,29 +35,29 @@ function getCurrentPage(){
 
    const parts = hash.split('/');
    return parseInt(parts[parts.findIndex((p) => p === 'page') + 1]);
-};
+}
 
 function showCurrentPage(data, container){
    const curPage = getCurrentPage();
    populateHomepage(data, curPage, pageSize, container);
-};
+}
 
 function changePage(targetPage, value, filteredData){
-
    let maxPages = 0;
 
    fetch(stories).then((response) => response.json()).then((data) => {
-      (value)? maxPages = Math.ceil(data.stories.length / pageSize) : maxPages = Math.ceil(filteredData.length / pageSize);
-      
-      if (targetPage < 0){
+      value
+         ? (maxPages = Math.ceil(data.stories.length / pageSize))
+         : (maxPages = Math.ceil(filteredData.length / pageSize));
+
+      if (targetPage < 0) {
          targetPage = maxPages - 1;
-      } else if (targetPage >= maxPages){
+      } else if (targetPage >= maxPages) {
          targetPage = 0;
       }
 
       document.location = document.location.search + `#/page/${targetPage}`;
       pagNums.forEach((el) => {
-
          if (parseInt(el.innerText) === getCurrentPage() + 1) {
             el.classList.add('active');
          } else {
@@ -65,6 +65,6 @@ function changePage(targetPage, value, filteredData){
          }
       });
    });
-};
+}
 
-export { showCurrentPage, getCurrentPage, changePage};
+export { showCurrentPage, getCurrentPage, changePage };
