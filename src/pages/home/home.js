170bc496c9ba6a125cdf363377878c1d301bd600
David Pavlovski <david.pavlovski@hotmail.com>
2022-07-06 21:25:57 +0200

    changed results text

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,7 +6,7 @@ const mainArticle = document.querySelector('#main-article');
 const prev = document.querySelector('#btn-previous');
 const next = document.querySelector('#btn-next');
 const pagNums = document.querySelectorAll('.pagination-numbers');
-const hideForSearch = document.querySelectorAll('.hide-for-search')
+const hideForSearch = document.querySelectorAll('.hide-for-search');
 const stories = '../../data/news.json';
 const title = document.querySelector('#title');
 let dataLength = 0;
@@ -21,23 +21,23 @@ function getData(stories){
       })
       .then(function({ stories }){
          let allStories = [ ...stories ];
-         let data = allStories.slice(allStories.length-50);
+         let data = allStories.slice(allStories.length - 50);
          if (key === 'category') {
-            title.innerText = `Showing results for ${value} category`;
+            title.innerText = `Showing results for "${value}" category`;
             data = [ ...stories.filter((x) => x.category.includes(value)) ];
-         } else if (key === 'search'  && value !== '') {
-            title.innerText = `Your search for: ${value} returned following results`;
+         } else if (key === 'search' && value !== '') {
+            title.innerText = `Your search for: "${value}" returned following results`;
             data = [
                ...(stories.filter((x) => x.title.toLowerCase().includes(value.toLowerCase())) ||
                   x.description.toLowerCase().includes(value.toLowerCase()))
             ];
-            if(data.length===0){
-               title.innerText = `Your search for: ${value} returned no results`;
+            if (data.length === 0) {
+               title.innerText = `Your search for: "${value}" returned no results`;
             }
-         } else if(value === ''){
+         } else if (value === '') {
             title.innerText = `Please enter a valid search value`;
-            hideForSearch.forEach(el => el.style.display = 'none');
-         };
+            hideForSearch.forEach((el) => (el.style.display = 'none'));
+         }
          showCurrentPage(data, articles, key === 'category' || key === 'search' ? null : mainArticle);
          dataLength = data.length;
       })
@@ -47,17 +47,17 @@ function getData(stories){
 }
 
 next.addEventListener('click', () => {
-    changePage(getCurrentPage() + 1, dataLength);
+   changePage(getCurrentPage() + 1, dataLength);
 });
- 
+
 prev.addEventListener('click', () => {
-    changePage(getCurrentPage() - 1, dataLength);
+   changePage(getCurrentPage() - 1, dataLength);
 });
- 
+
 pagNums.forEach((el) => {
-    el.addEventListener('click', () => {
-       changePage(el.innerText - 1, dataLength);
-    });
+   el.addEventListener('click', () => {
+      changePage(el.innerText - 1, dataLength);
+   });
 });
 
 getData(stories);
