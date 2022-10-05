4727b6acc9a9a752395394ef24b5a14d61b1a056
David Pavlovski <david.pavlovski@hotmail.com>
2022-10-05 22:50:04 +0200

    env changed

--- react-client/src/api/WeatherAPI.js
+++ react-client/src/api/WeatherAPI.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
-const API_KEY = '2c02fbaa40e80adfed940892d3d56de0';
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 export const getCityName = async (coords) => {
 	const ednpoint = `${BASE_URL}/geo/1.0/reverse?lat=${coords.latitude}&lon=${coords.longitude}&limit=1&appid=${API_KEY}`;
 	const res = await axios.get(ednpoint);
