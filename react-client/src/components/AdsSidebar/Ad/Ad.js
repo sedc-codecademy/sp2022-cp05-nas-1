ac68cd998480c600799e9b578a6a237626afd8f9
David Pavlovski <david.pavlovski@hotmail.com>
2022-10-13 19:05:10 +0200

    added default ad images if no active ads are present

--- react-client/src/components/AdsSidebar/Ad/Ad.js
+++ react-client/src/components/AdsSidebar/Ad/Ad.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import useAds from '../../../hooks/useAds';
+import { Container } from '../../Panel/Panel.styles';
 function Ad() {
 	const { ads } = useAds();
 	const ad = ads[Math.floor(Math.random() * ads.length)];
+	if (!ad?.imageUrl) {
+		return (
+			<img
+				src='https://growtraffic-bc85.kxcdn.com/blog/wp-content/uploads/2016/10/Sell-Ads-on-a-Website.jpg'
+				alt='space for your ad'
+			/>
+		);
+	}
 	return (
 		<a href={ad.redirectUrl} target='_blank'>
 			<img src={ad.imageUrl} alt='Advertisement' />
